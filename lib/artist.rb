8b74f89aca3c2ebfcf8e5277145edda0d72fac4a
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-08-08 02:59:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,13 @@ end
     @@artists
   end 
   
-  def add_song(song)
+   def add_song(song)
     @songs << song
+    song.artist = self
+  end
+
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
   end 
 end 
