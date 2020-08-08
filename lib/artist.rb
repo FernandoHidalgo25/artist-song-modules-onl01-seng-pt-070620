6b47eb194c973977bb8faccde0a9e84a78cdf82b
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-08-08 02:58:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,29 @@
-require 'pry'
-
 class Artist
-
+  attr_accessor :name
+  attr_reader :songs
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   extend Findable
-  include PARAMABLE
-  
-  attr_accessor :name
-  attr_reader :songs
+  include Paramable
 
-  @songs = []
-  @@artists = [] 
-  
+  @@artists = []
+
   def initialize
-  self.class.all << self
-end
-  
-  def self.all 
+    super
+    @songs = []
+  end
+
+  def self.all
     @@artists
-  end 
-  
+  end
+
   def add_song(song)
     @songs << song
-  end 
-end 
+    song.artist = self
+  end
+
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
+
+end
